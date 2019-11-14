dc9cdc49e95f89e82503acaf9967c8808eb9a147
Minson <mitgic92@gmail.com>
2019-11-14 20:13:28 +0800

    second stage

---
+++ day15/day15.js
@@ -0,0 +1,6 @@
+function change(){
+    let box  = document.getElementById("move-transition-box");
+    box.style.height =200 + 'px';
+    box.style.width = 200 +'px';
+    box.style.backgroundColor = 'red';
+}
